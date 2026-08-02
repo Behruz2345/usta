911b28ecb04334f085fcb971ab428834e6a428a9
Ergashboyev Bexruz <rraxmonovarushana@gmail.com>
2026-08-02 15:38:33 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 
 # Telegram bot tokeningizni @BotFather orqali oling va shu yerga qo'ying
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "TOKEN_NI_BU_YERGA_QOYING")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8916769188:AAFkNdPm2xcZGjdwyUoHzDaA7j6m6ngXiAg")
 
 # Sizning (administratorning) Telegram chat ID raqamingiz.
 # Uni bilish uchun @userinfobot ga /start yozing.
-ADMIN_CHAT_ID = os.environ.get("ADMIN_CHAT_ID", "123456789")
+ADMIN_CHAT_ID = os.environ.get("ADMIN_CHAT_ID", "8893508552")
 
 # "Administrator" tugmasi bosilganda ochiladigan shaxsiy Telegram profilingiz
 # (@ belgisisiz, masalan: "john_doe")
-ADMIN_TELEGRAM_USERNAME = os.environ.get("ADMIN_TELEGRAM_USERNAME", "your_username")
+ADMIN_TELEGRAM_USERNAME = os.environ.get("ADMIN_TELEGRAM_USERNAME", "alidacru")
 
 # Flask session uchun maxfiy kalit
-SECRET_KEY = os.environ.get("SECRET_KEY", "juda-maxfiy-kalit-buni-ozgartiring")
+SECRET_KEY = os.environ.get("SECRET_KEY", "ernfowrufh4785fg48fc")
 
 # Ilova haqida matn
 APP_NAME = "AvtoUsta"
